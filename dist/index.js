ba950b4e4008acced255bbb045303944ecae553e
Brian Soumakian <bsoumakian@gmail.com>
2016-09-29 21:14:19 -0700

    0.0.2

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -8,26 +12,24 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-(function (Phaser) {
-  var GreenhousePlugin = function (_Phaser$Plugin) {
-    _inherits(GreenhousePlugin, _Phaser$Plugin);
+var GreenhousePlugin = function (_Phaser$Plugin) {
+  _inherits(GreenhousePlugin, _Phaser$Plugin);
 
-    function GreenhousePlugin() {
-      _classCallCheck(this, GreenhousePlugin);
+  function GreenhousePlugin() {
+    _classCallCheck(this, GreenhousePlugin);
 
-      return _possibleConstructorReturn(this, (GreenhousePlugin.__proto__ || Object.getPrototypeOf(GreenhousePlugin)).apply(this, arguments));
+    return _possibleConstructorReturn(this, (GreenhousePlugin.__proto__ || Object.getPrototypeOf(GreenhousePlugin)).apply(this, arguments));
+  }
+
+  _createClass(GreenhousePlugin, [{
+    key: 'configure',
+    value: function configure(config) {
+      this.gameid = config.id;
+      this.firebase = config.firebase.child('games').child(this.gameid);
     }
+  }]);
 
-    _createClass(GreenhousePlugin, [{
-      key: 'configure',
-      value: function configure(config) {
-        this.gameid = config.id;
-        this.firebase = config.firebase.child('games').child(this.gameid);
-      }
-    }]);
+  return GreenhousePlugin;
+}(Phaser.Plugin);
 
-    return GreenhousePlugin;
-  }(Phaser.Plugin);
-
-  Phaser.Plugin.Greenhouse = GreenhousePlugin;
-})(Phaser);
+exports.default = GreenhousePlugin;
