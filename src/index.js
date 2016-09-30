fabfa9dc5a29ee7479e6ad98a24010896cd6dc9a
Brian Soumakian <bsoumakian@gmail.com>
2016-09-29 22:26:45 -0700

    update plugin

--- src/index.js
+++ src/index.js
@@ -9,15 +9,6 @@ module.exports = class GreenhousePlugin extends Phaser.Plugin {
 
     const assetPath = config.assetPath || '/';
     this.assetPath = assetPath.lastIndexOf('/') === assetPath.length-1 ? assetPath : assetPath + '/';
-
-    this.adUrl = config.adUrl || '';
-  }
-
-  initalize() {
-    // enable ads plugin
-    this.game.add.plugin(Fabrique.Plugins.AdManager);
-    const provider = new Fabrique.AdProvider.Ima3(this.game, this.adUrl);
-    this.game.ads.setAdProvider(provider);
   }
 
   loadAtlas() {
