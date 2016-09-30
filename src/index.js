4949d5d0b4a6f8c955b1901b6552ce87ff4292ab
Brian Soumakian <bsoumakian@gmail.com>
2016-09-29 22:54:19 -0700

    add firebase integration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import rsvp from 'rsvp';
+
 module.exports = class GreenhousePlugin extends Phaser.Plugin {
   constructor(game, parent) {
     super(game, parent);
@@ -5,7 +7,7 @@ module.exports = class GreenhousePlugin extends Phaser.Plugin {
 
   configure(config) {
     this.name = config.name;
-    this.firebase = config.firebase;
+    this._firebase = config.firebase;
 
     const assetPath = config.assetPath || '/';
     this.assetPath = assetPath.lastIndexOf('/') === assetPath.length-1 ? assetPath : assetPath + '/';
@@ -13,5 +15,25 @@ module.exports = class GreenhousePlugin extends Phaser.Plugin {
 
   loadAtlas() {
     this.game.load.atlas(this.name, this.assetPath + this.name + '.png', this.assetPath + this.name + '.json');
+  }
+
+  waitForAuth() {
+    const auth = this.firebase.auth();
+    const promise = new rsvp.Promise((resolve) => {
+      const callback = () => {
+        off();
+        resolve();
+      };
+      const off = auth.onAuthStateChanged(callback);
+    });
+    return promise;
+  }
+
+  firebaseRef(path) {
+    return this._firebase.database().ref('games').child(this.name);
+  }
+
+  firebaseAuth() {
+    return this._firebase.auth();
   }
 }
