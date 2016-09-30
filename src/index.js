81f87fae0c3d46aaaf2d097b009cf3135a79a73b
Brian Soumakian <bsoumakian@gmail.com>
2016-09-29 21:24:39 -0700

    fix module definition

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-class GreenhousePlugin extends Phaser.Plugin {
+module.exports = class GreenhousePlugin extends Phaser.Plugin {
+  constructor(game, parent) {
+    super(game, parent);
+  }
+
   configure(config) {
     this.gameid = config.id;
     this.firebase = config.firebase.child('games').child(this.gameid);
   }
 }
-
-export default GreenhousePlugin;
