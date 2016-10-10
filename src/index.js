62dd2c4f9a3d98f802e25f73b27df1e82c3ea63e
Brian Soumakian <bsoumakian@gmail.com>
2016-10-10 12:37:12 -0700

    remove firebase queries

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import firebase from 'firebase';
-import rsvp from 'rsvp';
 import Auth from './auth';
 
 module.exports = class GreenhousePlugin extends Phaser.Plugin {
   constructor(game, parent) {
     super(game, parent);
-
-    this.gamePlayedListener = null;
   }
 
   initialize(config) {
@@ -59,61 +55,11 @@ module.exports = class GreenhousePlugin extends Phaser.Plugin {
     this.game.load.atlas(this.name, this.assetPath + this.name + '.png', this.assetPath + this.name + '.json');
   }
 
-  onGamePlayed(cb) {
-    this.offGamePlayed();
-    let query = this.refData().orderByChild('endedAt');
-
-    query.limitToLast(1).once('value', (snapshot) => {
-      const games = snapshot.val();
-      const keys = games ? Object.keys(games) : null;
-
-      if (keys && keys.length > 0) {
-        query = query.startAt(games[keys[0]].endedAt + 1);
-      }
-
-      // setup listener
-      query.on('child_added', (snap) => cb(snap.val()));
-      this.gamePlayedListener = query;
-    });
-  }
-
-  offGamePlayed() {
-    if (this.gamePlayedListener) {
-      this.gamePlayedListener.off('child_added');
-    }
-    this.gamePlayedListener = null;
-  }
-
-  saveGamePlayed(data) {
-    const origKeys = Object.keys(data);
-    const gamedata = {
-      endedAt: firebase.database.ServerValue.TIMESTAMP,
-      uid: this.auth.currentUserUID(),
-      mode: this.mode,
-      name: this.name,
-      played: 1
-    };
-    origKeys.forEach((key) => {
-      gamedata[key] = data[key];
-    });
-
-    const promise = new rsvp.Promise((resolve, reject) => {
-      const promises = [];
-      promises.push(this.refData().push().set(gamedata));
-      promises.push(this.reporting.saveMetrics(gamedata));
-
-      rsvp.all(promises).then(resolve).catch(reject);
-    });
-    return promise;
-  }
-
   refData() {
-    return this.firebase.database().ref('data');
+    return this.firebase.database().ref('games').child(this.name).child('data');
   }
 
-  destroy() {
-    this.offGamePlayed();
-
-    super.destroy();
+  refReporting() {
+    return this.firebase.database().ref('games').child(this.name).child('reporting');
   }
 };
